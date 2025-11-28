79f5c91b3a462e248732418d2122622773cdb909
ianjwellings@gmail.com <ianjwellings@gmail.com>
2025-11-28 20:05:17 +0000

    Copy template files to design folder

--- AlibreGateDesignAutomation/DesignTemplates/TallGardenGate.cs
+++ AlibreGateDesignAutomation/DesignTemplates/TallGardenGate.cs
@@ -1,35 +1,71 @@
-﻿using System;
+﻿using AlibreX;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using AlibreX;
 
 namespace AlibreGateDesignAutomation.DesignTemplates
 {
     internal class TallGardenGate
     {
+        static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+        {
+            // Get information about the source directory
+            var dir = new DirectoryInfo(sourceDir);
+
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+            // Cache directories before we start copying
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Create the destination directory
+            Directory.CreateDirectory(destinationDir);
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string targetFilePath = Path.Combine(destinationDir, file.Name);
+                file.CopyTo(targetFilePath);
+            }
+
+            // If recursive and copying subdirectories, recursively call this method
+            if (recursive)
+            {
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                    CopyDirectory(subDir.FullName, newDestinationDir, true);
+                }
+            }
+        }
 
         public static void AlibreAutomation(int height, int width)
         {
             IAutomationHook hook;
             try
             {
-                //TAKE A COPY OF THE TEMPLATE FILE FOR EDITING
-                //THE LINEAR PATTERN DOESN'T LIKE A QTY GREATER THAN THE EXISTING?
+                //Delete existing design folder if it exists
+                Directory.Delete(@"C:\\Files\\MiM\\Design", true);
 
-
+                //Take a copy of the template folder for editing
+                //The linear pattern in Alibre model doesn't like a qty greater than the existing qty
+                CopyDirectory(@"C:\\Files\\MiM\\Templates\\TallGardenGate", @"C:\\Files\\MiM\\Design", true);
+      
                 //CAD file paths
-                string oAssembly1 = @"C:\\Files\\MiM\\Templates\\TallGardenGate\\TallGardenGate.AD_ASM";
-                string oPart1 = @"C:\\Files\\MiM\\Templates\\TallGardenGate\\25x5_FLAT_001.AD_PRT";
-                string oPart2 = @"C:\\Files\\MiM\\Templates\\TallGardenGate\\25x5_FLAT_002.AD_PRT";
-                string oPart3 = @"C:\\Files\\MiM\\Templates\\TallGardenGate\\25x25x2.5_SHS_001.AD_PRT";
-                string oPart4 = @"C:\\Files\\MiM\\Templates\\TallGardenGate\\25x25x2.5_SHS_002.AD_PRT";
-                string oPart5 = @"C:\\Files\\MiM\\Templates\\TallGardenGate\\25x25x2.5_SHS_003.AD_PRT";
-                string oPart6 = @"C:\\Files\\MiM\\Templates\\TallGardenGate\\12_ROUND_001.AD_PRT";
-                string oPart7 = @"C:\\Files\\MiM\\Templates\\TallGardenGate\\12_ROUND_002.AD_PRT";
+                string oAssembly1 = @"C:\\Files\\MiM\\Design\\TallGardenGate.AD_ASM";
+                string oPart1 = @"C:\\Files\\MiM\\Design\\25x5_FLAT_001.AD_PRT";
+                string oPart2 = @"C:\\Files\\MiM\\Design\\25x5_FLAT_002.AD_PRT";
+                string oPart3 = @"C:\\Files\\MiM\\Design\\25x25x2.5_SHS_001.AD_PRT";
+                string oPart4 = @"C:\\Files\\MiM\\Design\\25x25x2.5_SHS_002.AD_PRT";
+                string oPart5 = @"C:\\Files\\MiM\\Design\\25x25x2.5_SHS_003.AD_PRT";
+                string oPart6 = @"C:\\Files\\MiM\\Design\\12_ROUND_001.AD_PRT";
+                string oPart7 = @"C:\\Files\\MiM\\Design\\12_ROUND_002.AD_PRT";
 
                 //CAD input parameters
                 double gate_height = 0.1 * height; //Set input value and convert mm to cm
@@ -49,9 +85,6 @@ namespace AlibreGateDesignAutomation.DesignTemplates
                     double oTopRailheadInitialQty = Convert.ToInt32(Math.Floor((gate_width - 2.5) / 10));
                     double oTopRailheadActualQty = Convert.ToInt32(oTopRailheadInitialQty + 2);
                     double oTopRailheadSpacing = Math.Round((gate_width - 2.5) / (oTopRailheadActualQty - 1), 1);
-                    MessageBox.Show(Convert.ToString(oTopRailheadInitialQty));
-                    MessageBox.Show(Convert.ToString(oTopRailheadActualQty));
-                    MessageBox.Show(Convert.ToString(oTopRailheadSpacing));
                     double oTotalGap = oFlatWidth % 9;
                     double oRingQty = Convert.ToInt32(Math.Floor(oFlatWidth / 9));
                     double oRingGap = oTotalGap / (oRingQty + 1);
@@ -94,6 +127,13 @@ namespace AlibreGateDesignAutomation.DesignTemplates
                     part5.Parameters.Item("oBoxHeight").Value = oBoxHeight;
                     part5.Parameters.CloseParameterTransaction();
                     part5.Close(true);
+
+                    //Open part5 file and edit parameters
+                    IADPartSession part6 = (IADPartSession)root.OpenFile(oPart6);
+                    part6.Parameters.OpenParameterTransaction();
+                    part6.Parameters.Item("oTopBarLength").Value = oBoxHeight - 87.5;
+                    part6.Parameters.CloseParameterTransaction();
+                    part6.Close(true);
 
 
                     //TEST MERGING THE BELOW INTO ONE TRANSACTION
